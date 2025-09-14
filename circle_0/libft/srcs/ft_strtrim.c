953a6cc1c952268df58441163b3246ad1d34580e
funnyhun <funnyhun0222@gmail.com>
2025-09-14 17:30:40 +0900

    feat : circle_0_project

---
+++ circle_0/libft/srcs/ft_strtrim.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minhulee <minhulee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 14:51:03 by minhulee          #+#    #+#             */
+/*   Updated: 2023/10/14 12:37:48 by minhulee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	unsigned int	start;
+	unsigned int	end;
+
+	if (!s1 || !set)
+		return (ft_strdup(""));
+	start = 0;
+	while (s1[start])
+	{
+		if (!ft_strchr(set, s1[start]))
+			break ;
+		start++;
+	}
+	end = ft_strlen(s1);
+	while (end > 0)
+	{
+		if (!ft_strchr(set, s1[end - 1]))
+			break ;
+		end--;
+	}
+	if (end < start)
+		return (ft_strdup(""));
+	return (ft_strcut(s1, start, end));
+}
